b831a45bb6aa1bc04e33983e371a5dd1ad87ea69
Niek van de Pas <Niek_pas@live.nl>
2020-05-11 21:16:52 +0200

    chore: line endings make me sad

--- extension.ts
+++ extension.ts
@@ -1,99 +1,99 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import { window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument } from 'vscode';
 
 // this method is called when your extension is activated. activation is
 // controlled by the activation events defined in package.json
 export function activate(ctx: ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "Wordcount" is now active!');
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "Wordcount" is now active!');
 
-    // create a new word counter
-    let wordCounter = new WordCounter();
-    let controller = new WordCounterController(wordCounter);
+	// create a new word counter
+	let wordCounter = new WordCounter();
+	let controller = new WordCounterController(wordCounter);
 
-    // add to a list of disposables which are disposed when this extension
-    // is deactivated again.
-    ctx.subscriptions.push(controller);
-    ctx.subscriptions.push(wordCounter);
+	// add to a list of disposables which are disposed when this extension
+	// is deactivated again.
+	ctx.subscriptions.push(controller);
+	ctx.subscriptions.push(wordCounter);
 }
 
 export class WordCounter {
 
-    private _statusBarItem: StatusBarItem;
+	private _statusBarItem: StatusBarItem;
 
-    public updateWordCount() {
-        
-        // Create as needed
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-        } 
+	public updateWordCount() {
 
-        // Get the current text editor
-        let editor = window.activeTextEditor;
-        if (!editor) {
-            this._statusBarItem.hide();
-            return;
-        }
+		// Create as needed
+		if (!this._statusBarItem) {
+			this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+		}
 
-        let doc = editor.document;
+		// Get the current text editor
+		let editor = window.activeTextEditor;
+		if (!editor) {
+			this._statusBarItem.hide();
+			return;
+		}
 
-        // Only update status if an MD file
-        if (doc.languageId === "markdown") {
-            let wordCount = this._getWordCount(doc);
+		let doc = editor.document;
 
-            // Update the status bar
-            this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : '$(pencil) 1 Word';
-            this._statusBarItem.show();
-        } else {
-            this._statusBarItem.hide();
-        }
-    }
+		// Only update status if an MD file
+		if (doc.languageId === "markdown") {
+			let wordCount = this._getWordCount(doc);
 
-    public _getWordCount(doc: TextDocument): number {
-        let docContent = doc.getText();
+			// Update the status bar
+			this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : '$(pencil) 1 Word';
+			this._statusBarItem.show();
+		} else {
+			this._statusBarItem.hide();
+		}
+	}
 
-        // Parse out unwanted whitespace so the split is accurate
-        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
-        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-        let wordCount = 0;
-        if (docContent != "") {
-            wordCount = docContent.split(" ").length;
-        }
+	public _getWordCount(doc: TextDocument): number {
+		let docContent = doc.getText();
 
-        return wordCount;
-    }
+		// Parse out unwanted whitespace so the split is accurate
+		docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
+		docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+		let wordCount = 0;
+		if (docContent != "") {
+			wordCount = docContent.split(" ").length;
+		}
 
-    public dispose() {
-        this._statusBarItem.dispose();
-    }
+		return wordCount;
+	}
+
+	public dispose() {
+		this._statusBarItem.dispose();
+	}
 }
 
 class WordCounterController {
 
-    private _wordCounter: WordCounter;
-    private _disposable: Disposable;
+	private _wordCounter: WordCounter;
+	private _disposable: Disposable;
 
-    constructor(wordCounter: WordCounter) {
-        this._wordCounter = wordCounter;
-        this._wordCounter.updateWordCount();
+	constructor(wordCounter: WordCounter) {
+		this._wordCounter = wordCounter;
+		this._wordCounter.updateWordCount();
 
-        // subscribe to selection change and editor activation events
-        let subscriptions: Disposable[] = [];
-        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
-        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+		// subscribe to selection change and editor activation events
+		let subscriptions: Disposable[] = [];
+		window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+		window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
 
-        // create a combined disposable from both event subscriptions
-        this._disposable = Disposable.from(...subscriptions);
-    }
+		// create a combined disposable from both event subscriptions
+		this._disposable = Disposable.from(...subscriptions);
+	}
 
-    private _onEvent() {
-        this._wordCounter.updateWordCount();
-    }
+	private _onEvent() {
+		this._wordCounter.updateWordCount();
+	}
 
-    public dispose() {
-        this._disposable.dispose();
-    }
+	public dispose() {
+		this._disposable.dispose();
+	}
 }
